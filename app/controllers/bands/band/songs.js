e495bdb57c89a1eb4f3c717b1772e5c18a3a9788
Nathan <Burgess@Nathans-MBP.hotspot>
2015-10-24 17:12:35 -0400

    Star rating broken on set action

---
+++ app/controllers/bands/band/songs.js
@@ -0,0 +1,14 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+
+  actions: {
+    updateRating: function(params) {
+      var song = params.item;
+      var rating = params.rating;
+
+      song.set('rating', rating);
+    }
+  }
+
+});
