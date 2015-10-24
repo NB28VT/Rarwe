b12537a35dc680840b430bf04a8af9cd75e40872
Nathan <Burgess@Nathans-MacBook-Pro.local>
2015-10-24 00:58:16 -0400

    Working band list. Create bands NOT WORKING

---
+++ app/models/band.js
@@ -0,0 +1,23 @@
+import Ember from 'ember';
+
+export default Ember.Object.extend({
+  name: '',
+
+  init: function() {
+    this._super(...arguments);
+    if(!this.get('songs')) {
+      this.set('songs', []);
+    }
+  }
+
+  // setupSongs: Ember.on('init', function(){
+  //   if(!this.get('songs')) {
+  //     this.set('songs', []);
+  //   }
+  // }),
+
+  // slug: Ember.computed('name', function(){
+  //   return this.get('name').dasherize();
+  // }),
+
+});
--- app/routes/bands.js
+++ app/routes/bands.js
@@ -1,35 +1,39 @@
 import Ember from 'ember';
 
-var Band = Ember.Object.extend({
-  name: '',
+import Band from '../models/band';
+import Song from '../models/song';
 
-  slug: Ember.computed('name', function(){
-    return this.get('name').dasherize();
-  }),
-});
 
-var Song = Ember.Object.extend({
-  title: '',
-});
+// var Band = Ember.Object.extend({
+//   name: '',
+
+//   slug: Ember.computed('name', function(){
+//     return this.get('name').dasherize();
+//   }),
+// });
+
+// var Song = Ember.Object.extend({
+//   title: '',
+// });
 
 var blackDog = Song.create({
   title: "Black Dog",
-  band: "Led Zeppelin",
   rating: 3
 });
 
+
 var yellowLedbetter = Song.create({ title: 'Yellow Ledbetter',
-band: 'Pearl Jam',
-rating: 4
+  rating: 4
 });
+
 
 var daughter = Song.create({
   title: 'Daughter',
-  band: 'Pearl Jam',
   rating: 5
 });
 
-var pretender = Song.create({ title: 'The Pretender', band: 'Foo Fighters', rating: 2
+
+var pretender = Song.create({ title: 'The Pretender', rating: 2
 });
 
 var BandsCollection = Ember.Object.extend({
@@ -44,11 +48,24 @@ var pearlJam = Band.create({name: 'Pearl Jam', songs: [daughter, yellowLedbetter
 var fooFighters = Band.create({name: 'Foo Fighters', songs: [pretender]});
 
 
+
 var bands = BandsCollection.create();
+
 bands.get('content').pushObjects([ledZeppelin, pearlJam, fooFighters]);
+
 
 export default Ember.Route.extend({
   model: function (){
     return bands.get('sortedContent');
+  },
+
+
+  actions: {
+    createBand: function (){
+      var name = this.get('controller').get('name');
+      var band = Band.create({name: name});
+      bands.get('content').pushObject(band);
+      this.get('controller').set('name', '');
+    }
   }
 });
